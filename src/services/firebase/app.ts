650e94ba11a00adbd3d9aee7b75f49b3bcd838c0
Daniel Honorio <danielhonorio808@gmail.com>
2026-06-19 16:15:28 -0300

    Correção chave firebase

--- src/services/firebase/app.ts
+++ src/services/firebase/app.ts
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyD2W1GHiner13wuQseT94X1o1GS9F73Xx4",
-  authDomain: "promotor-fotos.firebaseapp.com",
-  projectId: "promotor-fotos",
-  storageBucket: "promotor-fotos.firebasestorage.app",
-  messagingSenderId: "820701754599",
-  appId: "1:820701754599:web:ef4c08e991d45c1b19de42",
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 
 export const firebaseApp = initializeApp(firebaseConfig);
